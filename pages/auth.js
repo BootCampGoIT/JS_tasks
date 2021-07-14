4dafe382890605d15c0c7228e3b095bc3d8b03fa
AndriiShnurkov <Shans12.84@ukr.net>
2021-07-14 10:00:02 +0300

    fix notation

--- pages/auth.js
+++ pages/auth.js
@@ -1,13 +1,17 @@
 // функция отвечает за возврат разметки страницы, которая будет
-// добавлена в main 
+// добавлена в main
 export const getAuthMarkup = () => {
-  return `<form class="authForm">
-    <label class="authLabel"
-      >Email<input type="email" autocomplete placeholder="Enter email"
-    /></label>
-    <label class="authLabel"
-      >Password<input type="password" autocomplete placeholder="Enter password"
-    /></label>
+  return `
+  <form class="authForm">
+    <label class="authLabel">
+      Email
+      <input type="email" autocomplete placeholder="Enter email"/>
+    </label>
+    <label class="authLabel">
+      Password
+      <input type="password" autocomplete placeholder="Enter password"/>
+    </label>
     <button>Auth</button>
-  </form>`;
+  </form>
+  `;
 };
